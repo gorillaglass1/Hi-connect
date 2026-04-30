4220ebde545d51e057f713587511f79c5813da14
현혜승 <hhsa602sa@gmail.com>
2026-04-30 18:43:43 +0900

    test
    # Conflicts:
    #	app/models/.idea/models.iml
    #	app/models/hydrogen_station.py

--- app/models/hydrogen_station.py
+++ app/models/hydrogen_station.py
@@ -1,3 +1,9 @@
+ HEAD
+
+from contextlib import nullcontext
+from decimal import Decimal
+
+ 5d653ba (test)
 from sqlalchemy import Column, Integer, String, TIMESTAMP, func
 
 from app.core.database import Base
@@ -5,3 +11,15 @@ from app.core.database import Base
 class hydrogen_station(Base):
     __tablename__ = 'hydrogen_station'
     hydrogen_station_id = Column(Integer,primary_key = True, autoincrement=True)
+HEAD
+
+    name = Column(String(100), nullable=  False)
+    address = Column(String(255), nullable = False)
+    latitude = Column(Decimal(10, 7), nullable = False)
+    longitude = Column(Decimal(10, 7), nullable = False)
+    contact_number = Column(String(30))
+    start_time = Column(TIME)
+    end_time = Column(TIME)
+    total_chargers = Column(Integer, defalt = 0)
+    payment_supported = Column(String(50))
+ 5d653ba (test)
