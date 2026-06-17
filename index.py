db0afd763255db94048476d8394e42cd9c116534
user <gudwn9669@naver.com>
2026-06-17 20:26:51 +0900

    back weather and traffic api

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 
 from app.api.charging_log_api import router as charging_log_router
-from app.api.ex_highway_api import router as ex_highway_router
 from app.api.hydrogen_station_api import router as hydrogen_station_router
 from app.api.hydrogen_station_facilities_api import (
     router as hydrogen_station_facilities_router,
@@ -60,7 +59,6 @@ app.include_router(recommendation_history_router)
 app.include_router(charging_log_router)
 app.include_router(user_preference_router)
 app.include_router(recommendation_router)
-app.include_router(ex_highway_router)
 
 
 @app.get("/")
